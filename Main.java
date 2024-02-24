a221dd0429eb679cf49b84525c88b25686d6d35c
jportil9 <152668693+jportil9@users.noreply.github.com>
2024-02-23 19:03:04 -0500

    Update Main.java
    ITS ALL DIFFERENT kind of

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 import java.io.*;
 import java.util.ArrayList;
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) throws IOException {
         int numRecipes = 0;
@@ -12,9 +13,10 @@ public class Main {
             recSearch.add(new Recipe(file));
         }
         numRecipes = recSearch.size();
-        FileWriter fw = new FileWriter("C:\\Users\\kpopc\\Desktop\\School\\Hackathon\\recipelist.txt");
+        FileWriter fw = new FileWriter("C:\\Users\\kpopc\\Desktop\\School\\Hackathon\\recipelist.json");
 
         //System.out.println("Testing recipe database...");
+        fw.write("{\n" + "\t\"recipes\": [\n");
         for(int i = 0 ; i < numRecipes ; i++)
         {
             //System.out.printf("Recipe: %s\nLink: %s\n", recSearch.get(i).getRecName(), recSearch.get(i).getRecLink());
@@ -33,12 +35,32 @@ public class Main {
             System.out.printf("Requires Microwave? %b\n", recSearch.get(i).getMicroRequirement());
             System.out.printf("Has Sharp Objects? %b\n", recSearch.get(i).getIfSharp());
             System.out.printf("Is a \"One-Dish\" Recipe? %b\n\n", recSearch.get(i).getIfOneDish());
-             */
+
             fw.write(recSearch.get(i).getRecName() + "|" + recSearch.get(i).getRecLink() + "||" + recSearch.get(i).getOvenRequirement() + "|" + recSearch.get(i).getStoveRequirement() + "|"
                     + recSearch.get(i).getSlowRequirement() + "|" + recSearch.get(i).getIPRequirement() + "|" + recSearch.get(i).getMicroRequirement() + "||"
                     + recSearch.get(i).getIfSharp() + "||" + recSearch.get(i).getIfOneDish());
             fw.write("\n");
+             */
+            fw.write("\t\t{\n");
+            fw.write("\t\t \"name\": \"" + recSearch.get(i).getRecName() + "\",\n");
+            fw.write("\t\t \"link\": \"" + recSearch.get(i).getRecLink() + "\",\n");
+            fw.write("\t\t \"oven\": \"" + recSearch.get(i).getOvenRequirement() + "\",\n");
+            fw.write("\t\t \"stove\": \"" + recSearch.get(i).getStoveRequirement() + "\",\n");
+            fw.write("\t\t \"slowcooker\": \"" + recSearch.get(i).getSlowRequirement() + "\",\n");
+            fw.write("\t\t \"instantpot\": \"" + recSearch.get(i).getIPRequirement() + "\",\n");
+            fw.write("\t\t \"microwave\": \"" + recSearch.get(i).getMicroRequirement() + "\",\n");
+            fw.write("\t\t \"sharp\": \"" + recSearch.get(i).getIfSharp() + "\",\n");
+            fw.write("\t\t \"onedish\": \"" + recSearch.get(i).getIfOneDish() + "\"\n");
+            if (i != numRecipes - 1)
+            {
+                fw.write("\t\t},\n");
+            }
+            else
+            {
+                fw.write("\t\t}");
+            }
         }
+        fw.write("\n\t]\n}");
         fw.close();
         System.out.printf("Total Recipes: %d\n\n", numRecipes);
 
